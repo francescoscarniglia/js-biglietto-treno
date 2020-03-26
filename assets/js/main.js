dc33a959df2aed4750e2e33b7dba53db923ccdb2
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-03-26 15:24:34 +0100

    Console ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
+// limiti di età per stabilire la comparazione
 var maggiorenne = 17;
 var overSixteen = 65;
-
 
 // 1° step quanti km dovrà percorrere l'utente?
 var kmUtente = parseInt(prompt('Inserisci i km che dovrai percorrere'));
@@ -13,17 +13,29 @@ var annoUtente = parseInt(prompt('Inserisci il tuo anno di nascita'));
 var dataAttuale = new Date();
 var anniUtente = dataAttuale.getFullYear() - annoUtente;
 console.log('I tuoi anni sono: ', anniUtente);
+
 // 3° step che prezzo avrà il biglietto?
-if(anniUtente < maggiorenne) {
-  console.log('Sei minorenne, hai uno sconto del 20%, quindi devi pagare: ', totaleCosto);
-} else if(anniUtente > overSixteen) {
-  console.log('Over 65 Devi pagare: ', totaleCosto );
-} else {
- console.log('Normal-price Devi pagare: ', totaleCosto );
-}
-
-// 4° step mostrare a schermo il prezzo del biglietto.
-
 // il prezzo del biglietto è definito in base ai km (0.21 al km), ma
 // va applicato  uno sconto del 20% per i minori anni 18 e
 // uno sconto del 40% per over 65
+
+if(anniUtente < maggiorenne) {
+  console.log('Prezzo Pieno:', totaleCosto);
+  var costoMinorenne = totaleCosto * 20 / 100;
+  console.log('Riduzione del 20%: ', costoMinorenne);
+  var totaleMinorenne = totaleCosto - costoMinorenne;
+  document.getElementById('name').innerHTML = totaleMinorenne;
+  console.log('Prezzo scontato: ', totaleCosto - costoMinorenne);
+} else if(anniUtente > overSixteen) {
+  console.log('Prezzo Pieno:', totaleCosto);
+  var costoMaggiorenne = totaleCosto * 40 / 100;
+  console.log('Riduzione del 40%: ', costoMaggiorenne);
+  var totaleMaggiorenne = totaleCosto - costoMaggiorenne;
+  console.log('Prezzo scontato: ', totaleCosto - costoMaggiorenne);
+  document.getElementById('name').innerHTML = totaleMaggiorenne;
+} else {
+  document.getElementById('name').innerHTML = totaleCosto;
+ console.log('Prezzo pieno: ', totaleCosto );
+}
+
+// 4° step mostrare a schermo il prezzo del biglietto.
